f1700de0849a784cbaa0c623271c139d7293d206
Alejo Velasco <alsoto2177@gmail.com>
2021-08-18 05:23:03 +0000

    Installation of Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,16 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [BlogController::class, 'index'])->name('blog.index');
-
-/*Blog resources */
-Route::get('blog/category/{slug}', [BlogController::class, 'category'])->name('blog.category');
-Route::get('blog/category/all/{slug}', [BlogController::class, 'categoryAll'])->name('blog.category_all');
-Route::resource('blog', BlogController::class)->except([
-    'index'
-]);
-
-
-Route::get('admin', function(){
-    return view('admin.dashboard');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';

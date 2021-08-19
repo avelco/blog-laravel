11e0e1b052be16cd3d2253ec3ccb706994cd185f
Alejo Velasco <alsoto2177@gmail.com>
2021-08-19 01:40:52 +0000

    Creation of controller for pages an posts, and creation of index view for posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.layout')
+@section('content')
+		<div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+			
+			<!--Console Content-->
+			
+			<div class="flex flex-wrap">
+
+            </div>
+								
+			<!--/ Console Content-->
+					
+		</div>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,9 @@
        <meta charset="UTF-8" />
        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+       <title> {{ config('app.name')}} </title>       
 
-        <title>Laravel</title>
+        
 
     </head>
 <body class="bg-gray-200 font-sans leading-normal tracking-normal">
